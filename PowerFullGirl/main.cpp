8f10bd52c10d43994702e1a846546dd4e4e73e26
Adeeb Mohammed <adeeb2358@gmail.com>
2020-04-21 09:56:09 +0900

    Initial Commit

---
+++ PowerFullGirl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PowerFullGirl
+//
+//  Created by adeeb mohammed on 21/04/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
