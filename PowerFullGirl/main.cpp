72c69ba0029491611a3e47feb5589c849b40496c
Adeeb Mohammed <adeeb2358@gmail.com>
2020-04-23 20:34:11 +0900

    no of powerful girls required for the eliminating evils

--- PowerFullGirl/main.cpp
+++ PowerFullGirl/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+#include <vector>
+
+long int calculateNoofPowerFulGirls(std::vector<long int> ingReqQty,
+                                    std::vector<long int> ingAvlQty){
+    
+    long int noOfGirls = LONG_MAX;
+    for(int i = 0; i < ingReqQty.size(); ++i){
+        long int currentQty = 0;
+        if(ingReqQty.at(i) > 0){
+            currentQty = ingAvlQty.at(i) / ingReqQty.at(i);
+            if (currentQty < noOfGirls) {
+                noOfGirls = currentQty;
+            }
+        }
+    }
+    return noOfGirls;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // the ingredients needed to create a powerfull girl
+    long int noOfIngredients = 0;
+    long int inqQty = 0;
+    std::cin >> noOfIngredients;
+    
+    // input ingredients quantity
+    std::vector<long int> ingReqQty(noOfIngredients);
+    // available quantity
+    std::vector<long int> ingAvlQty(noOfIngredients);
+    
+    for (int i = 0; i < noOfIngredients; i++) {
+        std::cin>> inqQty;
+        ingReqQty.push_back(inqQty);
+    }
+    
+    for (int i = 0; i < noOfIngredients; i++) {
+        std::cin>> inqQty;
+        ingAvlQty.push_back(inqQty);
+    }
+    
+    std::cout << calculateNoofPowerFulGirls(ingReqQty, ingAvlQty);
+    
     return 0;
 }
